8c442b5e78e3ee074214661c99087d367ecf5d95
Daniel Enrique High Mendoza <55213696+Daniel-High-4585@users.noreply.github.com>
2021-05-03 20:56:55 -0600

    V1.0.0

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-var mongoose = require('mongoose');
-var schema = require('./schema');
+var mongoose = require("mongoose");
+var schema = require("./schema");
 
-mongoose.connect('mongodb//localhost:2701/eje05');
+mongoose.connect("mongodb://localhost:27017/eje05");
 
 //parametros: nombre del modelo, esquema y nombre de la collecion
-var Book = mongoose.model ('Book',schema,'books');  
+var Book = mongoose.model("Book", schema, "books");
 
-let book = new Book ({
-    title:'100 años de soledad',
-    author:'Gabriel Garcia Marquez ',
-    body: 'Cosa'
-
+let book = new Book({
+  title: "100 años de soledad",
+  author: "Gabriel Garcia Marquez ",
+  body: "lorem",
 });
 
-book.save(function(error){
-    if(error){//si entra aqui, es por que ubo un error
+book.save(function (error) {
+  if (error) {
+    //si entra aqui, es por que ubo un error
     console.log(error);
     process.exit(1);
-    }
-    console.log('El doc se a creado con Exito');
-    process.exit(0);
+  }
+  console.log("El doc se a creado con Exito");
+  process.exit(0);
 });
